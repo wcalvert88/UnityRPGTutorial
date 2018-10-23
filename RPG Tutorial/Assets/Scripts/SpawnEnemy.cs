7a81dd8efbec50fa80ad4932e7ad5100ee632c42
wcalvert88 <wcalvert88@gmail.com>
2018-10-23 16:38:11 -0400

    got enemy spawner to trigger battle scene

---
+++ RPG Tutorial/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnEnemy : MonoBehaviour {
+
+	[SerializeField] private GameObject enemyEncounterPrefab;
+
+	private bool spawning = false;
+
+	void Start() {
+		DontDestroyOnLoad(this.gameObject);
+
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+	private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+		if (scene.name == "Battle") {
+			if (this.spawning) {
+				Instantiate(enemyEncounterPrefab);
+			}
+			SceneManager.sceneLoaded -= OnSceneLoaded;
+			Destroy(this.gameObject);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player") {
+			this.spawning = true;
+			SceneManager.LoadScene("Battle");
+		}	
+	}
+}
