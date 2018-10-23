22ce4ea7cf59af353065b32d9133ca823ced8fda
wcalvert88 <wcalvert88@gmail.com>
2018-10-23 08:36:33 -0400

    initial actual commit

---
+++ RPG Tutorial/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour {
+	
+	public Button button;
+	void Start() {
+		button.onClick.AddListener(delegate {loadNextScene("Town"); });
+	}
+
+	void Update() {
+	}
+	public void loadNextScene(string sceneName) {
+		SceneManager.LoadScene(sceneName);
+	}
+}
---
+++ RPG Tutorial/Assets/Scripts/UnitStats.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour {
+
+	public float health;
+	public float mana;
+	public float attack;
+	public float magic;
+	public float defense;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
